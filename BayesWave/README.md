9e7075f5aaaede5db811cb562266d1ca77cc2dab
FrancescoSarandrea <fsarandrea94@gmail.com>
2023-08-29 11:18:35 +0200

    adding a directory containing the scripts used to run BayesWave

---
+++ BayesWave/README.md
@@ -0,0 +1,7 @@
+# Pre-processing of Glitches
+This repository contains the scripts used to reconstruct the Glitches with BayesWave.
+
+**H1.cache** contains the information about the .gwf file of the signal. It assumes that the file is in the same repository.
+
+**example_GW** runs the BayesWave pipeline. It outputs the reconstructed signal and graphs into the outputDir folder. 
+
---
+++ BayesWave/example_GW.sh
@@ -0,0 +1,9 @@
+repodir=/home/fsarandrea/anaconda3/envs/igwn-py39/bin
+start_time=1165491287.76172
+
+${repodir}/BayesWave --checkpoint  --ifo H1 --psdlength 4.0 --H1-cache H1.cache --H1-channel H1:GWOSC-4KHZ_R1_STRAIN --bayesLine  --updateGeocenterPSD  --waveletPrior  --Dmax 100 --glitchOnly  --Niter 1000 --trigtime $start_time --segment-start $start_time --srate 1024.0 --seglen 4.0 --window 1.0 --H1-flow 30.0 --psdstart $start_time --outputDir outputDir2 --dataseed 1234
+
+${repodir}/BayesWavePost --ifo H1 --psdlength 4.0 --0noise  --bayesLine  --dataseed 1234 --trigtime $start_time --segment-start $start_time --srate 1024.0 --seglen 4.0 --window 1.0 --H1-flow 30.0 --psdstart $start_time --outputDir outputDir --H1-cache interp:outputDir/H1_fairdraw_asd.dat
+
+${repodir}/megaplot.py outputDir
+
